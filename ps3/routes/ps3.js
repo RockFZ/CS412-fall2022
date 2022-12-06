4b385dcb8c043404cf9ee86663d240f61e4d0afc
RockFZ <57330428+RockFZ@users.noreply.github.com>
2022-12-05 23:14:36 -0500

    revised ps3

--- ps3/routes/ps3.js
+++ ps3/routes/ps3.js
@@ -5,17 +5,31 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
     // Return our render pug template
     res.render('ps3', { stringToDisplay: 'Hey Now!' });
-})
-
-    .post('/', function(req, res, next) {
-
-    // Return and render it
-    res.render('post', { newString: (JSON.stringify(req.body)), lengthOfNewString: JSON.stringify(req.body).length });
-})
-
-    .get('/names/:name', function(req, res, next) {
-    // Return our render pug template
-
-        res.render('post', { aName: name });
 });
+
+router.get('/post', function(req, res){
+    let body = req.body;
+    console.log(body);
+    // let newString = body["string"];
+    // console.log(newString);
+    // let lengthOfNewString = body["length"];
+    console.log('String displayed');
+    res.end();
+});
+
+router.post('/', function(req, res, next) {
+    let body = req.body;
+    let newString = body["string"];
+    let lengthOfNewString = newString.length;
+    console.log(newString + ' ' + lengthOfNewString);
+    res.render('ps3', {stringToDisplay: JSON.stringify(newString), length: lengthOfNewString});
+    // res.send({string: newString, length: lengthOfNewString});
+});
+
+router.get('/names/:name', function(req, res, next) {
+    // Return our render pug template
+    res.render('post', { aName: req.params.name });
+});
+
+
 module.exports = router;
